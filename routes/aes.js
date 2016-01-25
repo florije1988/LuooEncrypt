00d8dba9c5610e6f90a3784bfa1196e8400c2e87
florije1988 <florije1988@gmail.com>
2016-01-25 18:02:27 +0800

    do not know why wrong.

--- routes/aes.js
+++ routes/aes.js
@@ -5,11 +5,20 @@ var express = require('express');
 var CryptoJS = require("crypto-js");
 var router = express.Router();
 
-/* GET users listing. */
+///* GET users listing. */
+//router.get('/', function(req, res, next) {
+//    var pass = req.query.pass;
+//    var word = req.query.word;
+//    var cryptedStr = CryptoJS.AES.decrypt('b6ce159334e155d8', 'U2FsdGVkX19c5m+90/fvNRsbZ9z9tZIZ+Y7tFi4UhCo=').toString(CryptoJS.enc.Utf8);
+//    //var cryptedStr = CryptoJS.AES.encrypt(word, pass).toString();
+//    res.json({data: cryptedStr});
+//});
+
 router.get('/:pass/:word', function(req, res, next) {
     var pass = req.params.pass;
     var word = req.params.word;
     var decryptedStr = CryptoJS.AES.decrypt(word, pass);
+    console.log(decryptedStr);
     // CryptoJS.AES.encrypt("florije", key).toString();
     res.json({data: decryptedStr.toString(CryptoJS.enc.Utf8)});
 });
