66641d5c70ebaee51bf8de56f9ed32ee8ca036f5
florije1988 <florije1988@gmail.com>
2016-01-26 11:11:52 +0800

    ok very good to solve the encrypt method.

--- routes/aes.js
+++ routes/aes.js
@@ -5,22 +5,22 @@ var express = require('express');
 var CryptoJS = require("crypto-js");
 var router = express.Router();
 
-///* GET users listing. */
-//router.get('/', function(req, res, next) {
-//    var pass = req.query.pass;
-//    var word = req.query.word;
-//    var cryptedStr = CryptoJS.AES.decrypt('b6ce159334e155d8', 'U2FsdGVkX19c5m+90/fvNRsbZ9z9tZIZ+Y7tFi4UhCo=').toString(CryptoJS.enc.Utf8);
-//    //var cryptedStr = CryptoJS.AES.encrypt(word, pass).toString();
-//    res.json({data: cryptedStr});
-//});
-
-router.get('/:pass/:word', function(req, res, next) {
-    var pass = req.params.pass;
-    var word = req.params.word;
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    var pass = req.query.pass;
+    var word = req.query.word;
     var decryptedStr = CryptoJS.AES.decrypt(word, pass);
-    console.log(decryptedStr);
-    // CryptoJS.AES.encrypt("florije", key).toString();
-    res.json({data: decryptedStr.toString(CryptoJS.enc.Utf8)});
+    //var cryptedStr = CryptoJS.AES.encrypt(word, pass).toString();
+    res.send(decryptedStr.toString(CryptoJS.enc.Utf8));
 });
+
+//router.get('/:pass/:word', function(req, res, next) {
+//    var pass = req.params.pass;
+//    var word = req.params.word;
+//    var decryptedStr = CryptoJS.AES.decrypt('U2FsdGVkX1+sQXIL46FIkFV72HxH3HBKFkbSlR4UZ60=', pass);
+//    console.log(decryptedStr);
+//    // CryptoJS.AES.encrypt("florije", key).toString();
+//    res.json({data: decryptedStr.toString(CryptoJS.enc.Utf8)});
+//});
 
 module.exports = router;
